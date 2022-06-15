06c4fc8304c5c0172d938c45e86161192a0baf84
Eddy L O Jansson <eddy@klopper.net>
2022-06-15 12:00:59 +0200

    Add self-compress/decompress test.
    This provides a larger input file, which would have exposed
    the code word buffer overflow immediately.

--- run-tests.sh
+++ run-tests.sh
@@ -23,8 +23,18 @@ function testfile {
 	test "$(sha256sum $TMPFILEC)" = "$HASHC  $TMPFILEC" || (echo "Test failed. -- Compressed hash mismatch" && exit 1)
 }
 
+function testcheck {
+	INFILE=$1
+	HASHD=$(sha256sum $INFILE | cut -f 1 -d ' ')
+	./lzw-eddy -c $INFILE -o $TMPFILEC
+	./lzw-eddy -d $TMPFILEC -o $TMPFILED
+	HASHC=$(sha256sum $TMPFILED | cut -f 1 -d ' ')
+	test "$HASHD" = "$HASHC" || (echo "Test failed. -- Compressed hash mismatch" && exit 1)
+}
+
 testfile tests/atsign.lzw "" c3641f8544d7c02f3580b07c0f9887f0c6a27ff5ab1d4a3e29caf197cfc299ae
 testfile tests/abra.txt.lzw "" 3119a48c6843ee7dcc08312e97b1d8e3b241b082996afe761f8a045d493b7cef
 testfile tests/zeros80000.lzw "" f8c784aa6b57396e7c5e094c34d079d8252473e46e2f60593a921dbebf941fcc
 testfile tests/zeros80000.lzw "-m 254" f8c784aa6b57396e7c5e094c34d079d8252473e46e2f60593a921dbebf941fcc 58e6c0321a62f7f112e61dca779edc9be0e34cf0ee356f61df949c7aea839492
+testcheck lzw.h
 echo "All tests passed."
