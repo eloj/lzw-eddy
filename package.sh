e5a13e67f0b3eff8abc684c41fb06e85ab72a058
Eddy L O Jansson <eddy@klopper.net>
2022-06-19 17:40:43 +0200

    Add msys2 Windows build

--- package.sh
+++ package.sh
@@ -2,8 +2,8 @@
 #
 # Create a release package for a specific OS.
 #
-
 OS=$1
+ARCH=${2:-`uname -m`}
 BP="packages/${OS}"
 RP="${BP}/lzw-eddy"
 FILES='lzw-eddy lzw.h LICENSE'
@@ -13,13 +13,12 @@ if [ -z "${OS}" ]; then
 	exit 1
 fi
 
-
-if [ "${OS}" == "linux" ]; then
-	echo "Packaging for ${OS}"
+if [[ "${OS}" == "linux" || "${OS}" == "windows" ]]; then
+	echo "Packaging for ${OS}-${ARCH}"
 	OPTIMIZED=1 make -B lzw-eddy
 	mkdir -p ${RP}
 	cp ${FILES} ${RP}
-	tar -C ${BP} -czvf packages/lzw-eddy.linux-amd64.tar.gz lzw-eddy
+	tar -C ${BP} -czvf packages/lzw-eddy.${OS}-${ARCH}.tar.gz lzw-eddy
 	rm -r ${BP}
 else
 	echo "Unknown target OS: ${OS}"
